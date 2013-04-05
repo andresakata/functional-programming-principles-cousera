fdff5fe3ed07b5a49501c5d04bbfee4b24dc9571
André Guimarães Sakata <andregsakata@gmail.com>
2013-04-04 21:38:36 -0300

    Clean code.

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -27,20 +27,11 @@ object Main {
     if (chars.isEmpty) true
 
     def verifier(acc: Int, chars: List[Char]): Boolean = {
-      if (chars.isEmpty) {
-        acc == 0
-      } else {
-        if (acc < 0) false
-        else {
-          if (chars.head == '(') {
-            verifier(acc + 1, chars.tail)
-          } else if (chars.head == ')') {
-            verifier(acc - 1, chars.tail)
-          } else {
-            verifier(acc, chars.tail)
-          }
-        }
-      }
+      if (chars.isEmpty) acc == 0
+      else if (acc < 0) false
+      else if (chars.head == '(') verifier(acc + 1, chars.tail)
+      else if (chars.head == ')') verifier(acc - 1, chars.tail)
+      else verifier(acc, chars.tail)
     }
 
     verifier(0, chars);
@@ -53,9 +44,6 @@ object Main {
     if (money == 0) 1
     else if (money < 0) 0
     else if (coins.isEmpty) 0
-    else {
-      countChange(money - coins.head, coins) +
-        countChange(money, coins.tail)
-    }
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
